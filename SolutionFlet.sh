59e595c06899f232c2f7ceab31a02888e5ff4af8
DarexD <126924966+DarexD@users.noreply.github.com>
2024-08-03 14:40:55 -0400

    Add files via upload

---
+++ SolutionFlet.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio libmpv-dev mpv -y
+
+sudo ln -s /usr/lib/x86_64-linux-gnu/libmpv.so /usr/lib/libmpv.so.1
+
+echo "You can now test your program ;D"
+echo "Ya puedes probar tu programa;D"
